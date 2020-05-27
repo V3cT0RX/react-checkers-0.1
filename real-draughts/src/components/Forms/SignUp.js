7d5528b73b7dcbb4785bacd30b892a69bb888978
Karan <karananantpure@gmail.com>
2020-05-27 13:13:44 +0530

    1]SignUP.js updated: 	functions are added for accepting value from form feild 	password and confirem password validetion is added.

--- real-draughts/src/components/Forms/SignUp.js
+++ real-draughts/src/components/Forms/SignUp.js
@@ -1,19 +1,38 @@
 import React from 'react';
-import {Navbar,Form, Button,Card} from 'react-bootstrap';
+import {Navbar,Form, Button,Card,Col} from 'react-bootstrap';
 
 
 class SignUp extends React.Component {
 
     constructor(props){
         super(props);
-        this.state = {email: null, password: null};
+        this.state = {email: null, password: null, firstName: '', lastName: '', confirmPassword: null};
         this.onChange = this.onChange.bind(this);
         this.onClick = this.onClick.bind(this);
+        this.confirmPasswordHandler = this.confirmPasswordHandler.bind(this);
+        this.firstNameHandler =this.firstNameHandler.bind(this);
+        this.lastNameHandler =this.lastNameHandler.bind(this);
     };
 
     onChange = e => {
         this.setState({[e.target.type]: e.target.value});
     }
+
+    confirmPasswordHandler = e => {
+        if (e.target.value !== this.state.password){
+            console.log("wrong");
+        }else{
+            this.setState({confirmPassword: e.target.value});
+        }
+    }
+
+    firstNameHandler = e => {
+            this.setState({firstName: e.target.value});
+        }
+    
+    lastNameHandler = e => {
+            this.setState({lastName: e.target.value});
+        }
 
     onClick(){
         console.log("on CLick", this.state);
@@ -38,16 +57,20 @@ class SignUp extends React.Component {
             <Card.Body>
                 <Card.Text>
                 <form>
+                <Form.Row>
+                <Col>
                 <Form.Group controlId="formBasicFname">
                     <Form.Label>First name</Form.Label>
-                    <Form.Control type="text" className="form-control" placeholder="First name" />
+                    <Form.Control type="text" className="form-control" onChange={this.firstNameHandler} placeholder="First name" />
                 </Form.Group>
-
+                </Col>
+                <Col>
                 <Form.Group controlId="formBasicLname">
                     <Form.Label>Last name</Form.Label>
-                    <Form.Control type="text" className="form-control" placeholder="Last name" />
+                    <Form.Control type="text" className="form-control" onChange={this.lastNameHandler} placeholder="Last name" />
                 </Form.Group>
-
+                </Col>
+                </Form.Row>
                 <Form.Group controlId="formBasicEmail">
                 <Form.Label>Email address</Form.Label>
                 <Form.Control type="email" onChange={this.onChange} placeholder="Enter email" />
@@ -63,7 +86,7 @@ class SignUp extends React.Component {
 
                 <Form.Group controlId="formBasicPassward">
                 <Form.Label>Confirm Password</Form.Label>
-                <Form.Control type="password" onChange={this.onChange} placeholder="confirm password"/>
+                <Form.Control type="password" onChange={this.confirmPasswordHandler} placeholder="confirm password"/>
                 </Form.Group>
 
                 <Button variant="primary" type="submit" onClick={this.onClick}>
